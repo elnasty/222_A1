0b13c847b4749936f6f3c7b861d62013802cbaf9
LNST\Le Buff <elnastyteo@gmail.com>
2018-01-30 16:35:52 +0800

    Test commit push

---
+++ newClass.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   newClass.cpp
+ * Author: Le Buff
+ * 
+ * Created on 30 January, 2018, 4:30 PM
+ */
+
+#include "newClass.h"
+
+newClass::newClass() {
+}
+
+newClass::newClass(const newClass& orig) {
+}
+
+newClass::~newClass() {
+}
+
