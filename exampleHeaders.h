4c204a2a8b5e172d278fa0b680573c0fd856c50b
LNST\Le Buff <elnastyteo@gmail.com>
2018-02-12 18:11:37 +0800

    Removed unrelated default project files, created example file containing example file and function headers

---
+++ exampleHeaders.h
@@ -0,0 +1,63 @@
+// -*- C++ -*-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * Name         : exampleHeader.h
+ * Author       : Stanley
+ * Sub Authors  :
+ *
+ * Created on   : 120217
+ *
+ * Purpose      : contains example file header and function header for keeping
+ *                coding style consistent amongst teammates
+ * 
+ * Changes      :
+ * DATE         USER            DETAIL
+ * 120217       Stanley         Original file conception
+ */
+
+#ifndef EXAMPLEHEADERS_H
+#define EXAMPLEHEADERS_H
+
+
+
+#endif /* EXAMPLEHEADERS_H */
+
+// function declarations
+void example1();
+int example2 (int);
+
+/*
+ * Name         : void example1()
+ * Author       : Stanley
+ * 
+ * Inputs       : void
+ * Returns      : void
+ * 
+ * Purpose      : an example function header
+ */
+void example1()
+{
+    // i do absolutely nothing
+    return;
+}
+
+/*
+ * Name         : int example2(int)
+ * Author       : Stanley
+ * 
+ * Inputs       : 1 int
+ * Returns      : 1 int
+ * 
+ * Purpose      : another example function header
+ */
+int example2(int)
+{
+    // i also do absolutely nothing
+    return 0;
+}
+
